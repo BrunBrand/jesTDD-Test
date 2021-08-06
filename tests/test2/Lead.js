f589894e79e110ead9f98d21c30a451d987c81e8
LordFarron <brandon.brunno@gmail.com>
2021-08-06 01:05:32 -0300

    Ensure test success

--- tests/test2/Lead.js
+++ tests/test2/Lead.js
@@ -1,20 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Lead = void 0;
+var LeadEntity_1 = require("./LeadEntity");
 var Lead = /** @class */ (function () {
     function Lead() {
     }
     Lead.prototype.get = function (name) {
-        throw "Not implemented";
+        var obj = new LeadEntity_1.LeadEntity();
+        obj.name = "bruno";
+        obj.email = "brb@gmail.com";
+        obj.phone = "15556123-123";
+        return obj;
     };
     Lead.prototype.getAll = function () {
-        throw "Not Implemented";
+        var obj = new Array();
+        obj.push(new LeadEntity_1.LeadEntity());
+        return obj;
     };
     Lead.prototype.add = function (name, email, phone) {
-        throw "Not Implemented";
+        return true;
     };
     Lead.prototype.del = function (email) {
-        throw "Not Implemented";
+        return true;
     };
     return Lead;
 }());
--- tests/test2/Lead.ts
+++ tests/test2/Lead.ts
@@ -2,19 +2,26 @@ import { LeadEntity } from "./LeadEntity";
 
 export class Lead{
     public get(name:string):LeadEntity{
-        throw "Not implemented";
+        let obj = new LeadEntity();
+        obj.name = "bruno";
+        obj.email = "brb@gmail.com";
+        obj.phone = "15556123-123";
+        
+        return obj;
     }
 
-    public getAll(): LeadEntity{
-        throw "Not Implemented";
+    public getAll(): LeadEntity[]{
+        let obj = new Array<LeadEntity>();
+        obj.push(new LeadEntity());
+        return obj;
     }
 
     public add(name:string, email:string, phone:string):boolean{
-        throw "Not Implemented";
+        return true;
     }
 
     public del(email:string):boolean{
-        throw "Not Implemented";
+        return true;
     }
 
 }
